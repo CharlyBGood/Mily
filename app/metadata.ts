64de4cd1c46aa153e33216b718de018d41c2fb18
charlybgood__x <bonavita.carlos@gmail.com>
2025-07-06 17:43:34 -0300

    create metadata.ts file

---
+++ app/metadata.ts
@@ -0,0 +1,11 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mily",
+  description: "Mily",
+  icons: {
+    icon: "/placeholder-logo.svg",
+    shortcut: "/placeholder-logo.svg",
+    apple: "/placeholder-logo.svg"
+  }
+};
